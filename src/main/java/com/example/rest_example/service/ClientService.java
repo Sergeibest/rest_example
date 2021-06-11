989c12b98b67a7ae039378f5cac5dce90b40a250
sgubko <gubko0207@gmail.com>
2021-06-11 17:05:04 +0300

    ХЗ че за дичь, СС поправил в трех местах отвалилось

--- src/main/java/com/example/rest_example/service/ClientService.java
+++ src/main/java/com/example/rest_example/service/ClientService.java
@@ -1,53 +1,42 @@
 package com.example.rest_example.service;
 
-import com.example.rest_example.model.Client;
-import org.springframework.stereotype.Service;
+import ch.qos.logback.core.net.server.Client;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+public interface ClientService {
 
-@Service
-public class ClientServiceImpl implements ClientService {
+    /**
+     * Создает нового клиента
+     * @param client - клиент для создания
+     */
+    void create(Client client);
 
-    // Хранилище клиентов
-    private static final Map<Integer, Client> CLIENT_REPOSITORY_MAP = new HashMap<>();
+    /**
+     * Возвращает список всех имеющихся клиентов
+     * @return список клиентов
+     */
+    List<сlient> readAll();
 
-    // Переменная для генерации ID клиента
-    private static final AtomicInteger CLIENT_ID_HOLDER = new AtomicInteger();
+    /**
+     * Возвращает клиента по его ID
+     * @param id - ID клиента
+     * @return - объект клиента с заданным ID
+     */
+    Client read(int id);
 
-    @Override
-    public void create(Client client) {
-        final int clientId = CLIENT_ID_HOLDER.incrementAndGet();
-        client.setId(clientId);
-        CLIENT_REPOSITORY_MAP.put(clientId, client);
-    }
+    /**
+     * Обновляет клиента с заданным ID,
+     * в соответствии с переданным клиентом
+     * @param client - клиент в соответсвии с которым нужно обновить данные
+     * @param id - id клиента которого нужно обновить
+     * @return - true если данные были обновлены, иначе false
+     */
+    boolean update(Client client, int id);
 
-    @Override
-    public List<Client> readAll() {
-        return new ArrayList<>(CLIENT_REPOSITORY_MAP.values());
-    }
-
-    @Override
-    public Client read(int id) {
-        return CLIENT_REPOSITORY_MAP.get(id);
-    }
-
-    @Override
-    public boolean update(Client client, int id) {
-        if (CLIENT_REPOSITORY_MAP.containsKey(id)) {
-            client.setId(id);
-            CLIENT_REPOSITORY_MAP.put(id, client);
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean delete(int id) {
-        return CLIENT_REPOSITORY_MAP.remove(id) != null;
-    }
+    /**
+     * Удаляет клиента с заданным ID
+     * @param id - id клиента, которого нужно удалить
+     * @return - true если клиент был удален, иначе false
+     */
+    boolean delete(int id);
 }
+
